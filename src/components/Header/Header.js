af9d9e813b27dc61dd4fc115758f3b2f5ea667d9
asrafaliii <asraf.bookaand@gmail.com>
2022-03-26 01:45:30 +0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Laptop Express</h1>
+            <p>Grab your dream laptops at an affordable price. Buy HP, Acer, Dell, Lenovo, Asus</p>
+        </div>
+    );
+};
+
+export default Header;
